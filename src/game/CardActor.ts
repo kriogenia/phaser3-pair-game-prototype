3c71d2e529e3d28d95d45468b28a0eb8e518182d
Krio <uo265710@uniovi.es>
2021-01-29 01:47:48 +0100

    Complete game

--- src/game/CardActor.ts
+++ src/game/CardActor.ts
@@ -1,15 +1,42 @@
 import Phaser from 'phaser'
+import BoardScene from '~/scenes/BoardScene';
 import TextureKeys from '~/consts/TextureKeys';
+
+const HIDDEN_FRAME = 13;
 
 export default class CardActor extends Phaser.GameObjects.Sprite
 {
 
 	/* Card id */
-	private id!: number;
+	id!: number;
+	/* Card state */
+	private isPressed: boolean = false;
 
 	constructor(scene: Phaser.Scene, x: number, y: number, id: number)
 	{
-		super(scene, x, y, TextureKeys.Cards, id);
+		super(scene, x, y, TextureKeys.Cards, HIDDEN_FRAME);
 		this.id = id;
+		/* Set mouse input */
+		this.setInteractive().on('pointerdown', this.onPress);
 	}
+
+	/**
+	 * Event OnClick. Sets the card as pressed and notifies the board
+	 */
+	onPress = () => {
+		if (!this.isPressed) {
+			this.isPressed = true;
+			this.setFrame(this.id);
+			(this.scene as BoardScene).cardPressed(this);
+		}
+	}
+
+	/**
+	 * Unselects and hides the card
+	 */
+	hide = () => {
+		this.isPressed = false;
+		this.setFrame(HIDDEN_FRAME);
+	}
+
 }
--- src/scenes/BoardScene.ts
+++ src/scenes/BoardScene.ts
@@ -7,8 +7,10 @@ const DEFAULT_MAX_COLUMNS = 4;
 
 export default class BoardScene extends Phaser.Scene
 {
-
-	cards: CardActor[] = new Array();
+	/* Pointer to selected card */
+	selected? : CardActor;
+	/* Game attributes */
+	pairsToMatch = DEFAULT_BOARD_SIZE / 2;
 	rows: number = Math.floor(DEFAULT_BOARD_SIZE / DEFAULT_MAX_COLUMNS);
 
 	constructor()
@@ -22,7 +24,7 @@ export default class BoardScene extends Phaser.Scene
 		this.load.spritesheet(TextureKeys.Cards, 'img/cards.png', { 
 			frameWidth: 81, 
 			frameHeight: 117, 
-			endFrame: 13 
+			endFrame: 14 
 		});
     }
 
@@ -32,13 +34,65 @@ export default class BoardScene extends Phaser.Scene
 		this.add.image(this.scale.width * 0.5, this.scale.height * 0.5, 'background');
 		/* Create the cards */
 		this.generateCards();
-		
+		/* Hide context menu */
+		this.input.mouse.disableContextMenu();
+	}
+
+	/**
+	 * Card pressed event. Checks if the player made a pair or not and calls for the consequent action
+	 * @param pressed 	Card pressed
+	 */
+	cardPressed = (pressed: CardActor) : void => 
+	{
+		// Second pressed card
+		if (this.selected) {
+			(this.selected.id === pressed.id) 
+				? this.matchPair() 										// Cards match
+				: setTimeout(() => this.hideCards(pressed), 500);		// Cards do not match
+		}
+		// First pressed card
+		else {
+			this.selected = pressed;
+		}
+	} 
+
+	/**
+	 * Sets the pair as matched and checks for the win
+	 */
+	private matchPair = () : void => 
+	{
+		console.log("Pair match");
+		this.selected = undefined;
+		this.pairsToMatch--;
+		this.checkWin();
+	}
+
+	/**
+	 * Hides both selected cards and unselects them
+	 * @param pressed 	Last selected card
+	 */
+	private hideCards = (pressed: CardActor) : void => 
+	{
+		console.log(pressed);
+		this.selected?.hide();
+		pressed.hide();
+		this.selected = undefined;
+	}
+
+	/**
+	 * Checks and notifies when the player wins
+	 */
+	private checkWin = () => 
+	{
+		if (!this.pairsToMatch) {
+			setTimeout(() => alert("You win"), 100);
+		}
 	}
 
 	/**
 	 * Fill the Cards array with pairs of cards
 	 */
-	private generateCards() : void
+	private generateCards = () : void =>
 	{
 		const width = this.scale.width;
 		const height = this.scale.height;
@@ -47,7 +101,6 @@ export default class BoardScene extends Phaser.Scene
 			const x = i % DEFAULT_MAX_COLUMNS * Math.floor(width / (DEFAULT_MAX_COLUMNS + 1)) + Math.floor(width / (DEFAULT_MAX_COLUMNS + 1));
 			const y = Math.floor(i / DEFAULT_MAX_COLUMNS) * Math.floor(height / (this.rows + 1)) + Math.floor(height / (this.rows + 1));
 			const card = new CardActor(this, x, y, ids[i]);
-			this.cards.push(card);
 			this.add.existing(card);
 		}
 	}
@@ -55,7 +108,7 @@ export default class BoardScene extends Phaser.Scene
 	/**
 	 * Generates a shuffle array of duplicated numbers
 	 */
-	private generateIds() : number[]
+	private generateIds = () : number[] =>
 	{
 		const ids = new Array();
 		/* Create the ids array */
